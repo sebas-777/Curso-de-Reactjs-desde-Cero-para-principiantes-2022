18e62ad6f78fce0cb5777cf150a06e9177d850c3
Sebastian Castro <scastro@qvision.com.co>
2023-12-15 19:46:35 -0500

    second commit

--- taller/src/app/page.js
+++ taller/src/app/page.js
@@ -1,95 +1,44 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+
+// Componentes funcionales 
+function Saludar(props) {
+  return<h1> are you bitch {props.name}?</h1>
+  
+} 
+
+//Componentes funcionales
+function Chat() {
+  return(
+    <form>
+      <input type ="text" placeholder='Son of a bitch'></input>
+      <input type = "submit" value={"Send"}></input>
+    </form>
+  )
+  
+}
+
+//Componente funcional 
+function MyButton() { 
+  return(
+    <button>
+      Soy un boton
+    </button>
+  );
+  
+}
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <> 
+    <h1>Hello Asshole!</h1> 
+    <Saludar name ="saul"></Saludar>
+    <Saludar name ="juan"></Saludar>
+    <Chat></Chat>
+    <div>
+      <MyButton />
+    </div>
+    </>
   )
 }
