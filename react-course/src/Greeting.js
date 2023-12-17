1d8d75d891184dfc5a29936c5f6c62cc9f966d1c
Sebastian Castro <scastro@qvision.com.co>
2023-12-17 16:08:05 -0500

    "se realiza minuto 57"

---
+++ react-course/src/Greeting.js
@@ -0,0 +1,18 @@
+
+
+export function Greeting() {
+    
+    const user ={
+      firsName : 'ryan',
+      lastname: 'RAY'
+      }
+      return<div>
+          <h1>{user.firsName}</h1>
+          <h3>{user.lastname}</h3>
+      </div>
+    
+  } 
+
+  export function UserCard(){
+    return <h1> User Card</h1>
+  }
---
+++ react-course/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import{Greeting, UserCard} from './Greeting'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(
+    <>
+    <Greeting/>
+    <UserCard/>
+    </>
+  
+    
+ 
+);
